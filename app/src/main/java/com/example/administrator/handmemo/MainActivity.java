1ee08070390c2ea962ef976b221a0f161c4baf35
Administrator <qskeksq@gmail.com>
2017-06-07 22:29:01 +0900

    commit

--- app/src/main/java/com/example/administrator/handmemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/handmemo/MainActivity.java
@@ -6,35 +6,106 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     FrameLayout layout;
+    RadioGroup radioGroup;
+    SeekBar seekBar;
+    DrawingBoard board;
+//    Path path;
+
+    int progress;
+    int color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-//        layout = new FrameLayout(this);
+        layout = (FrameLayout) findViewById(R.id.layout);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                switch (checkedId){
+                    case R.id.blue:
+                        color = Color.BLUE;
+//                        MainActivity.this.makePath();
+                        MainActivity.this.board.invalidate();
+                        break;
+                    case R.id.red:
+                        color = Color.RED;
+                        MainActivity.this.board.invalidate();
+                        break;
+                }
+            }
+        });
+        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
 
         // 1. 보드를 새로 생성한다.
-        DrawingBoard board = new DrawingBoard(this);
+        board = new DrawingBoard(this);
         // 2. 붓을 만들어서 보드에 담는다.
-        Paint paint = new Paint();
-        paint.setColor(Color.MAGENTA);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(1);
-        board.setPaint(paint);
+//        Paint paint = new Paint();
+//        paint.setColor(Color.MAGENTA);
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setStrokeWidth(progress);
+//        board.setPaint(paint);
         // 3. 생성된 보드를 화면에 세팅
-        setContentView(board);
-//        layout.addView(board);
+//        setContentView(board);
+        layout.addView(board);
+
     }
+
+    public void setStroke(int progress){
+        this.progress = progress;
+    }
+
+
+//    public void makePath(){
+//        path = new Path();
+//    }
+
+//    CompoundButton.OnClickListener clickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//                switch (v.getId()) {
+//                    case R.id.blue:
+//                        Toast.makeText(getBaseContext(), "확인", Toast.LENGTH_SHORT).show();
+//                        break;
+//
+//                }
+//        }
+//    };
+
+    SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//            Toast.makeText(getBaseContext(), "확인", Toast.LENGTH_SHORT).show();
+            MainActivity.this.setStroke(progress);
+            MainActivity.this.board.invalidate();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+//            MainActivity.this.board.invalidate();
+        }
+    };
 
     class DrawingBoard extends View {
 
@@ -43,20 +114,28 @@ public class MainActivity extends AppCompatActivity {
 
         public DrawingBoard(Context context){
             super(context);
-            path = new Path();
+//            path = new Path();
         }
 
         // 붓을 만드는
-        public void setPaint(Paint paint){
-            this.paint = paint;
-        }
+//        public void setPaint(Paint paint){
+//            this.paint = paint;
+//        }
 
         // 일단 그리는 일은 여기서 한다. 다른 일 하지 말 것!!
         @Override
         public void onDraw(Canvas canvas){
+            path = new Path();
+            paint = new Paint();
+            paint.setColor(color);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(progress);
+            paint.setAntiAlias(true);
             canvas.drawPath(path, paint);
         }
 
+        // 움직일 때마다 계속 onTouchEvent 메소드가 호출되고, UP 까지 값을 계속 갱신해 주는 것이로군.
+        // 로그값으로 어떻게 onTouchEvent 작동하는지 잘 살펴보자
         @Override
         public boolean onTouchEvent(MotionEvent event){
             // 내가 터치한 좌표를 꺼낸다.
@@ -64,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
             float y = event.getY();
 
             switch (event.getAction()){
+                // DOWN 에서 만들어 진 것이 MOVE 에도 쓰여야 하기 때문에 전역 변수로 선언한다.
                 case MotionEvent.ACTION_DOWN:
                     path.moveTo(x,y);  // 그리지 않고 이동한다
                     break;
